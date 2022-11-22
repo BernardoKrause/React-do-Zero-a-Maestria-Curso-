f2a18c85152a8227632fc35fc4c3546ae1f4f915
Bernardo Krause <bernardokrause@hotmail.com>
2022-11-21 22:52:07 -0300

    Criando Componentes

---
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -0,0 +1,16 @@
+// components
+import FirstComponent from './components/FirstComponent'
+
+// styles 
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos React</h1>
+      <FirstComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
@@ -0,0 +1,11 @@
+const FirstComponent = () => {
+
+    return (
+        <div>
+            <h1>Meu primeiro componente</h1>
+        </div>
+    )
+
+}
+
+export default FirstComponent

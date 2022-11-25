18da61e426c47afb23ae60adbed1ac3a2ad2b7f9
Bernardo Krause <bernardokrause@hotmail.com>
2022-11-25 00:04:19 -0300

    Função de Renderização

--- 2_FUNDAMENTOS/fundamentos/src/components/Events.js
+++ 2_FUNDAMENTOS/fundamentos/src/components/Events.js
@@ -4,6 +4,16 @@ const Events = () => {
         window.alert("Ativou o evento!")
     }
 
+    const renderSomething = (x) => {
+
+        if(x) {
+            return <h1>Rendezirando isso!</h1>
+        } else {
+            return <h1>Também posso renderizar isso!</h1>
+        }
+
+    }
+
     return (
         <div>
             <div>
@@ -17,6 +27,8 @@ const Events = () => {
                     }
                 }}>Clique aqui, por favor</button>
             </div>
+            {renderSomething(true)}
+            {renderSomething(false)}
         </div>
     )
 }

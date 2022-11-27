09883311d340788c0f2ed88033e37da79916bc96
Bernardo Krause <bernardokrause@hotmail.com>
2022-11-26 22:36:52 -0300

    Imagem em pública/src

--- 3_AVANÇANDO/avancando/src/App.js
+++ 3_AVANÇANDO/avancando/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 
+import Goats from './assets/goats.jpg'
+
 function App() {
   return (
-    <div>
+    <div className="App">
       <h1>Avançando em React</h1>
+      <div>
+        <img width="600px" src="/img1.jpg" alt="Paisagem" />
+      </div>
+      <div>
+        <img width="600px" src={Goats} alt="Carneiros" />
+      </div>
     </div>
   );
 }

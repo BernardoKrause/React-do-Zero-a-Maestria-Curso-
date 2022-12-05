2d2a6cd0b7fd38a0e9a4b829cbaa51bebb2f0bbf
Bernardo Krause <bernardokrause@hotmail.com>
2022-12-04 23:50:22 -0300

    CSS de componentes

--- 4_CSS_REACT/css/src/App.js
+++ 4_CSS_REACT/css/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import MyComponent from './components/MyComponent';
 
 function App() {
   return (
     <div className="App">
+      {/* CSS Global */}
       <h1>React com CSS</h1>
+      {/* CSS de componente */}
+      <MyComponent />
+      <p>Este parágrafo é do App.js</p>
     </div>
   );
 }
---
+++ 4_CSS_REACT/css/src/components/MyComponent.js
@@ -0,0 +1,13 @@
+import './MyComponent.css'
+
+const MyComponent = () => {
+  return (
+    <div>
+        <h1>CSS de componente</h1>
+        <p>Este é o parágrafo do componente</p>
+        <p className="my-comp-paragraph">Este tb é do componente</p>
+    </div>
+  )
+}
+
+export default MyComponent

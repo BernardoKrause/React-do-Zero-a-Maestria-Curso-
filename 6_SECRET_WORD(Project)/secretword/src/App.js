c01d1ca5ec7960b2b44c00d51ab7d7c45fe1e3e5
Bernardo Krause <bernardokrause@hotmail.com>
2022-12-18 15:05:20 -0300

    Finalizando tela inicial

--- 6_SECRET_WORD(Project)/secretword/src/App.js
+++ 6_SECRET_WORD(Project)/secretword/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import StartScreen from './components/StartScreen';
 
 function App() {
   return (
     <div className="App">
-      <h2>Secret Word</h2>
+      <StartScreen />
     </div>
   );
 }
---
+++ 6_SECRET_WORD(Project)/secretword/src/components/StartScreen.js
@@ -0,0 +1,13 @@
+import './StartScreen.css'
+
+const StartScreen = () => {
+  return (
+    <div className="start">
+        <h1>Secret World</h1>
+        <p>Clique no botão abaixo para começar a jogar</p>
+        <button>Começar o jogo</button>
+    </div>
+  )
+}
+
+export default StartScreen

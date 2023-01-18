243f84c2c5e250980f3afd529299fbc3bfa799be
Bernardo Krause <bernardokrause@hotmail.com>
2023-01-18 09:23:49 -0300

    Resgatando dados da API

--- 7_REQ_HTTP/httpreact/src/App.js
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -1,9 +1,34 @@
 import './App.css';
 
+import { useState, useEffect } from 'react';
+
+const url = "http://localhost:3000/products"
+
 function App() {
+  const [products, setProducts] = useState([]);
+
+  // 1 - resgatando dados
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
+
+      const data = await res.json();
+
+      setProducts(data);
+    }
+    
+    fetchData();
+  }, []);
+
+
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name} - R$ {product.price}</li>
+        ))}
+      </ul>
     </div>
   );
 }

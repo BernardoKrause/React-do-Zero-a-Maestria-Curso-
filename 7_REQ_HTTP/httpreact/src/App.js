03f63684521c26ec21bc8a3800eb7a3c100a3272
Bernardo Krause <bernardokrause@hotmail.com>
2023-01-23 08:20:14 -0300

    Refatorando o POST

--- 7_REQ_HTTP/httpreact/src/App.js
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -11,7 +11,7 @@ function App() {
   const [products, setProducts] = useState([]);
 
   // 4 - custom hook
-  const { data: items } = useFetch(url);
+  const { data: items, httpConfig } = useFetch(url);
 
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
@@ -38,18 +38,21 @@ function App() {
       price,
     };
 
-    const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(product),
-    });
+    //const res = await fetch(url, {
+    //  method: "POST",
+    //  headers: {
+    //    "Content-Type": "application/json"
+    //  },
+    //  body: JSON.stringify(product),
+    //});
 
     // 3 - carregamento
-    const addedProduct = await res.json()
+    //const addedProduct = await res.json()
 
-    setProducts((prevProducts) => [...prevProducts, addedProduct]);
+    //setProducts((prevProducts) => [...prevProducts, addedProduct]);
+
+    // 5 - refatorando post
+    httpConfig(product, "POST");
     
     setName("");
     setPrice("");
--- 7_REQ_HTTP/httpreact/src/hooks/useFetch.js
+++ 7_REQ_HTTP/httpreact/src/hooks/useFetch.js
@@ -1,8 +1,28 @@
 import { useState, useEffect } from "react";
 
+
 // 4 - custom hook
 export const useFetch = (url) => {
     const [data, setData] = useState(null)
+
+    // 5 - refatorando post
+    const [config, setConfig] = useState(null)
+    const [method, setMethod] = useState(null)
+    const [callFetch, setCallFetch] = useState(false)
+
+    const httpConfig = (data, method) => {
+        if(method === "POST") {
+            setConfig({
+                method,
+                headers: {
+                    "Content-type": "application/json"
+                },
+                body: JSON.stringify(data)
+            })
+
+            setMethod(method);
+        }
+    }
 
     useEffect(() => {
         const fetchData = async () => {
@@ -16,7 +36,26 @@ export const useFetch = (url) => {
 
         fetchData();
 
-    }, [url]);
+    }, [url, callFetch]);
 
-    return {data};
+    // 5 - refatorando post
+    useEffect(() => {
+        const httpRequest = async () => {
+            if(method === "POST") {
+
+                let fetchOptions = [url, config];
+    
+                const res = await fetch(...fetchOptions);
+    
+                const json = await res.json();
+    
+                setCallFetch(json);
+    
+            }
+        }
+
+        httpRequest();
+    }, [config, method, url])
+
+    return { data, httpConfig };
 };

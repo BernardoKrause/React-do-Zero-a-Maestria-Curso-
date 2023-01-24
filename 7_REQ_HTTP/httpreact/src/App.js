5ea99619783803df99c070ef371c11c1aeebf5e5
Bernardo Krause <bernardokrause@hotmail.com>
2023-01-23 22:22:35 -0300

    Estado de loading

--- 7_REQ_HTTP/httpreact/src/App.js
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -11,7 +11,7 @@ function App() {
   const [products, setProducts] = useState([]);
 
   // 4 - custom hook
-  const { data: items, httpConfig } = useFetch(url);
+  const { data: items, httpConfig, loading } = useFetch(url);
 
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
@@ -61,11 +61,13 @@ function App() {
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
-      <ul>
+      {/* 6 - loading */}
+      {loading && <p>Carregando dados...</p>}
+      {!loading && <ul>
         {items && items.map((product) => (
           <li key={product.id}>{product.name} - R$ {product.price}</li>
         ))}
-      </ul>
+      </ul>}
       <div className="add-product">
         <form onSubmit={handleSubmit}>
           <label>
--- 7_REQ_HTTP/httpreact/src/hooks/useFetch.js
+++ 7_REQ_HTTP/httpreact/src/hooks/useFetch.js
@@ -10,6 +10,9 @@ export const useFetch = (url) => {
     const [method, setMethod] = useState(null)
     const [callFetch, setCallFetch] = useState(false)
 
+    // 6 - loading
+    const [loading, setLoading] = useState(false);
+
     const httpConfig = (data, method) => {
         if(method === "POST") {
             setConfig({
@@ -26,12 +29,17 @@ export const useFetch = (url) => {
 
     useEffect(() => {
         const fetchData = async () => {
+
+            // 6 - loading
+            setLoading(true);
+
             const res = await fetch(url);
 
             const json = await res.json();
 
             setData(json);
 
+            setLoading(false);
         }
 
         fetchData();
@@ -57,5 +65,5 @@ export const useFetch = (url) => {
         httpRequest();
     }, [config, method, url])
 
-    return { data, httpConfig };
+    return { data, httpConfig, loading };
 };
